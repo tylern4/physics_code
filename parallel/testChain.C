e8415df31fb8ab48637be30d87c813d7a18f30ec
Nick Tyler <tylern@canisius.edu>
2015-06-11 13:04:26 -0400

    parallel

--- parallel/testChain.C
+++ parallel/testChain.C
@@ -4,9 +4,9 @@
     c->AddFileInfoList((TCollection*)fc.GetList());
     TProof::Open("workers=3");
     c->SetProof();
-    TStopwatch sw;
-    sw.Start();
+    //TStopwatch sw;
+    //sw.Start();
     c->Process("h10.C++"); //,"",1000000);
-    sw.Stop();
-    sw.Print();
+    //sw.Stop();
+    //sw.Print();
 }
