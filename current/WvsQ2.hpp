5481dcb5c977c4e67f47a69f2194f8ca3c43a7ba
Nick Tyler <tylern@canisius.edu>
2015-06-30 20:54:07 -0400

    remove extra hists

--- current/WvsQ2.hpp
+++ current/WvsQ2.hpp
@@ -74,23 +74,22 @@ void WvsQ2(char *fin, char *RootFile_output){
 
 		// Check to see whether the first particle is an Electron
 		// Changed id to id[0] because scattered elctron should be first particle (i.e. id[0])
-		if (id[0] == ELECTRON && gpart > 1 && stat[0] > 0 && (int)q[0] == -1 && cc[0] > 0 && sc[0] > 0 && dc[0] > 0 && ec[0] > 0 && dc_stat[dc[0]-1] > 0 ){
+		if ((id[0] == ELECTRON || id[0] == 0) && gpart > 1 && stat[0] > 0 && (int)q[0] == -1 && cc[0] > 0 && sc[0] > 0 && dc[0] > 0 && ec[0] > 0 && dc_stat[dc[0]-1] > 0 ){
 			//Setup scattered electron 4 vector
 			e_mu_prime_3.SetXYZ(p[0]*cx[0],p[0]*cy[0],p[0]*cz[0]);	
 			e_mu_prime.SetVectM(e_mu_prime_3, MASS_E);
-			//e_mu_prime.SetVectM(e_mu_prime_3, m[0]);
 
 			//Get energy of scattered elctron from 4 vector and calculate Q2 and W
 			WvsQ2_Fill(e_mu_prime.E(),W_calc(e_mu, e_mu_prime),Q2_calc(e_mu, e_mu_prime),xb_calc(Q2_calc(e_mu,e_mu_prime), e_mu_prime.E() ) );
-			total += e_mu_prime;	
-			#pragma omp parallel for
-			for(int event_number = 1; event_number < gpart; event_number++){
+			total += e_mu_prime;
+
+			//#pragma omp parallel for
+			for(int event_number = 0; event_number <= gpart; event_number++){
 				//Get particles 3 and 4 vector for current event.
 				TVector3 Particle3(0.0,0.0,0.0);
 				TLorentzVector Particle4(0.0,0.0,0.0,0.0);
 				Particle3.SetXYZ(p[event_number]*cx[event_number], p[event_number]*cy[event_number], p[event_number]*cz[event_number]);
 				Particle4.SetVectM(Particle3,Get_Mass(id[event_number]));
-				//Particle4.SetVectM(Particle3,m[event_number]);
 
 				MomVsBeta_Fill(Particle4.E(),Particle4.P(),b[event_number]);
 				total += Particle4;
@@ -100,7 +99,7 @@ void WvsQ2(char *fin, char *RootFile_output){
 					Fill_e_pi_found(W_calc(e_mu,e_mu_prime),Q2_calc(e_mu,e_mu_prime),Particle4.P(),b[event_number]);
 					//If Pi+ and Proton
 					//#pragma omp parallel for
-					for (int event_number_1 = 1; event_number_1 < gpart; event_number_1++){
+					for (int event_number_1 = 0; event_number_1 <= gpart; event_number_1++){
 						if(id[event_number_1] == PROTON && (int)q[event_number_1] == 1 /*&& sc[event_number_1] > 0 && dc[event_number_1] > 0*/) {
 							Fill_e_proton_pi_found(W_calc(e_mu,e_mu_prime),Q2_calc(e_mu,e_mu_prime),Particle4.P(),b[event_number]);
 							//total += Particle4;
@@ -109,11 +108,9 @@ void WvsQ2(char *fin, char *RootFile_output){
 				//If Proton	
 				} else if (id[event_number] == PROTON && (int)q[event_number] == 1 /*&& sc[event_number] > 0 && dc[event_number] > 0*/){
 					Fill_e_proton_found(W_calc(e_mu,e_mu_prime),Q2_calc(e_mu,e_mu_prime),Particle4.P(),b[event_number]);
-				} else if (id[event_number] == NEUTRON && (int)q[event_number] == 0 /*&& sc[event_number] > 0 && dc[event_number] > 0*/){
-					Fill_e_neutron_found(W_calc(e_mu,e_mu_prime),Q2_calc(e_mu,e_mu_prime),Particle4.P(),b[event_number]);
 				} 
 			} 
-			Fill_Missing_Mass((total-e_mu).M());
+			//Fill_Missing_Mass((total-e_mu).M());
 		}
 	}
 	chain.Reset();						// delete Tree object
