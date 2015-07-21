afec7ed169aad361f79ef7890aae3fee6ba99a69
Nick Tyler <tylern@canisius.edu>
2015-07-21 12:13:46 -0400

    cleanup and stuff

--- current/histo2.h
+++ current/histo2.h
@@ -21,17 +21,9 @@ float w_max_2 = 3.25;
 float q2_min_2 = 0;
 float q2_max_2 = 10;
 
-TH2D *WvsQ2_e_proton_pi_found = new TH2D("WvsQ2_e_proton_pi_found","W vs Q^{2} p #pi^{+}", bins_2, w_min_2, w_max_2, bins_2, q2_min_2, q2_max_2);
-TH1D *W_e_proton_pi_found = new TH1D("W_e_proton_pi_found","W p #pi^{+}",bins_2,  w_min_2, w_max_2);
-TH1D *Q2_e_proton_pi_found = new TH1D("Q2_e_proton_pi_found","Q^{2} p #pi^{+}",bins_2, q2_min_2, q2_max_2);
-
 TH2D *WvsQ2_e_proton_found = new TH2D("WvsQ2_e_proton_found","W vs Q^{2} p", bins_2, w_min_2, w_max_2, bins_2, q2_min_2, q2_max_2);
 TH1D *W_e_proton_found = new TH1D("W_e_proton_found","W p",bins_2,  w_min_2, w_max_2);
 TH1D *Q2_e_proton_found = new TH1D("Q2_e_proton_found","Q^{2} p",bins_2, q2_min_2, q2_max_2);
-
-//TH2D *WvsQ2_e_neutron_found = new TH2D("WvsQ2_e_neutron_found","W vs Q^{2} n", bins_2, w_min_2, w_max_2, bins_2, q2_min_2, q2_max_2);
-//TH1D *W_e_neutron_found = new TH1D("W_e_neutron_found","W n",bins_2,  w_min_2, w_max_2);
-//TH1D *Q2_e_neutron_found = new TH1D("Q2_e_neutron_found","Q^{2} n",bins_2, q2_min_2, q2_max_2);
 
 TH2D *WvsQ2_e_pi_found = new TH2D("WvsQ2_e_pi_found","W vs Q^{2} #pi^{+}", bins_2, w_min_2, w_max_2, bins_2, q2_min_2, q2_max_2);
 TH1D *W_e_pi_found = new TH1D("W_e_pi_found","W #pi^{+}",bins_2,  w_min_2, w_max_2);
@@ -43,9 +35,7 @@ float p_max = 2.5;
 float b_min = 0.1;
 float b_max = 1.2;
 
-TH2D *MomVsBeta_e_proton_pi_found = new TH2D("MomVsBeta_e_proton_pi_found","Momentum Vs #beta p #pi^{+}", bins_pvb, p_min, p_max, bins_pvb, b_min, b_max);
 TH2D *MomVsBeta_e_proton_found = new TH2D("MomVsBeta_e_proton_found","Momentum Vs #beta p", bins_pvb, p_min, p_max, bins_pvb, b_min, b_max);
-//TH2D *MomVsBeta_e_neutron_found = new TH2D("MomVsBeta_e_neutron_found","Momentum Vs #beta p", bins_pvb, p_min, p_max, bins_pvb, b_min, b_max);
 TH2D *MomVsBeta_e_pi_found = new TH2D("MomVsBeta_e_pi_found","Momentum Vs #beta #pi^{+}", bins_pvb, p_min, p_max, bins_pvb, b_min, b_max);
 
 TH1D *Missing_Mass = new TH1D("Missing_Mass", "Missing Mass", 500,-5.0,5.0);
@@ -54,12 +44,6 @@ void Fill_Missing_Mass(double mass){
 	Missing_Mass->Fill(mass);
 }
 
-void Fill_e_proton_pi_found(double W, double Q2,double p,double beta){
-	WvsQ2_e_proton_pi_found->Fill(W,Q2);
-	W_e_proton_pi_found->Fill(W);
-	MomVsBeta_e_proton_pi_found->Fill(p,beta);
-	Q2_e_proton_pi_found->Fill(Q2);
-}
 
 void Fill_e_proton_found(double W, double Q2,double p,double beta){
 	WvsQ2_e_proton_found->Fill(W,Q2);
@@ -67,13 +51,6 @@ void Fill_e_proton_found(double W, double Q2,double p,double beta){
 	MomVsBeta_e_proton_found->Fill(p,beta);
 	Q2_e_proton_found->Fill(Q2);
 }
-
-/*void Fill_e_neutron_found(double W, double Q2,double p,double beta){
-	WvsQ2_e_neutron_found->Fill(W,Q2);
-	W_e_neutron_found->Fill(W);
-	MomVsBeta_e_neutron_found->Fill(p,beta);
-	Q2_e_neutron_found->Fill(Q2);
-}*/
 
 void Fill_e_pi_found(double W, double Q2,double p,double beta){
 	WvsQ2_e_pi_found->Fill(W,Q2);
@@ -83,17 +60,6 @@ void Fill_e_pi_found(double W, double Q2,double p,double beta){
 }
 
 void Write_found_hists(){
-	WvsQ2_e_proton_pi_found->SetXTitle("W (GeV)");
-	WvsQ2_e_proton_pi_found->SetYTitle("Q^{2} (GeV^{2})");
-	W_e_proton_pi_found->SetXTitle("W (GeV)");
-	Q2_e_proton_pi_found->SetXTitle("Q^{2} (GeV^{2})");
-	MomVsBeta_e_proton_pi_found->SetXTitle("Momentum (GeV)");
-	MomVsBeta_e_proton_pi_found->SetYTitle("#beta");
-
-	MomVsBeta_e_proton_pi_found->Write();
-	//WvsQ2_e_proton_pi_found->Write();
-	//W_e_proton_pi_found->Write();
-	//Q2_e_proton_pi_found->Write();
 
 	WvsQ2_e_proton_found->SetXTitle("W (GeV)");
 	WvsQ2_e_proton_found->SetYTitle("Q^{2} (GeV^{2})");
@@ -103,21 +69,6 @@ void Write_found_hists(){
 	MomVsBeta_e_proton_found->SetYTitle("#beta");
 
 	MomVsBeta_e_proton_found->Write();
-	//WvsQ2_e_proton_found->Write();
-	//W_e_proton_found->Write();
-	//Q2_e_proton_found->Write();
-
-	//WvsQ2_e_neutron_found->SetXTitle("W (GeV)");
-	//WvsQ2_e_neutron_found->SetYTitle("Q^{2} (GeV^{2})");
-	//W_e_neutron_found->SetXTitle("W (GeV)");
-	//Q2_e_neutron_found->SetXTitle("Q^{2} (GeV^{2})");
-	//MomVsBeta_e_neutron_found->SetXTitle("Momentum (GeV)");
-	//MomVsBeta_e_neutron_found->SetYTitle("#beta");
-
-	//MomVsBeta_e_neutron_found->Write();
-	//WvsQ2_e_neutron_found->Write();
-	//W_e_neutron_found->Write();
-	//Q2_e_neutron_found->Write();
 
 	WvsQ2_e_pi_found->SetXTitle("W (GeV)");
 	WvsQ2_e_pi_found->SetYTitle("Q^{2} (GeV^{2})");
@@ -127,11 +78,7 @@ void Write_found_hists(){
 	MomVsBeta_e_pi_found->SetYTitle("#beta");
 
 	MomVsBeta_e_pi_found->Write();
-	//WvsQ2_e_pi_found->Write();
-	//W_e_pi_found->Write();
-	//Q2_e_pi_found->Write();
 
-	//Missing_Mass->Write();
 }
 
 #endif
