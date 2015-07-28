143e7bc81d0b67e14cacd5cc2e4a431e6fffd46e
Nick Tyler <tylern@canisius.edu>
2015-07-28 13:30:16 -0400

    The usual

--- current/missing_mass.hpp
+++ current/missing_mass.hpp
@@ -24,18 +24,25 @@ void missing_mass(TLorentzVector gamma_mu){
 			Temp_vec_3_PROTON.SetXYZ(p[event_number]*cx[event_number], p[event_number]*cy[event_number], p[event_number]*cz[event_number]);
 			Temp_vec_4_PROTON.SetVectM(Temp_vec_3_PROTON, MASS_P);
 			numOfProtons++;
-
-			for (int event_number_1 = 0; event_number_1 < gpart; event_number_1++) {
-				if (id[event_number_1] == PIP && (int)q[event_number_1] == 1) {
-					numOfPis++;
-					Temp_vec_3_PIP.SetXYZ(p[event_number_1]*cx[event_number_1], p[event_number_1]*cy[event_number_1], p[event_number_1]*cz[event_number_1]);
-					Temp_vec_4_PIP.SetVectM(Temp_vec_3_PIP, MASS_PIP);
-				}
-			}
+		} else if (id[event_number] == PIP && (int)q[event_number] == 1) {
+			numOfPis++;
+			Temp_vec_3_PIP.SetXYZ(p[event_number]*cx[event_number], p[event_number]*cy[event_number], p[event_number]*cz[event_number]);
+			Temp_vec_4_PIP.SetVectM(Temp_vec_3_PIP, MASS_PIP);
 		} 
 	}
 	if (numOfPis == 1 && numOfProtons == 1) Fill_Missing_Mass(missing_mass_calc(gamma_mu,Temp_vec_4_PROTON,Temp_vec_4_PIP));
-	if (numOfPis == 1 && numOfProtons == 1 && gpart == 3) Fill_Missing_Mass_P_PI(missing_mass_calc(gamma_mu,Temp_vec_4_PROTON,Temp_vec_4_PIP));
+	if (numOfPis == 1 && numOfProtons == 1 && gpart == 3) {
+		/*cout << red << "************" << endl;
+		cout << "gamma: ";
+		gamma_mu.Print();
+		cout << "proton: ";
+		Temp_vec_4_PROTON.Print();
+		cout << "pi: ";
+		Temp_vec_4_PIP.Print();
+		cout << green << missing_mass_calc(gamma_mu,Temp_vec_4_PROTON,Temp_vec_4_PIP) << endl; */
+
+		Fill_Missing_Mass_P_PI(missing_mass_calc(gamma_mu,Temp_vec_4_PROTON,Temp_vec_4_PIP));
+	}
 }
 
 #endif
