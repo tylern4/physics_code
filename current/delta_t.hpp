79b1b6ee72dc54835e5ad583da0a528eafa56611
Nick Tyler <tylern@canisius.edu>
2015-09-22 11:15:43 -0400

    Change it back!

--- current/delta_t.hpp
+++ current/delta_t.hpp
@@ -7,19 +7,9 @@
 
 #ifndef DELTA_T_H_GUARD
 #define DELTA_T_H_GUARD
-#include "main.h"
+
 const double c_special_units = 29.9792458;
 
-class delta_t
-{
-
-public:
-	static double vertex_time(double sc_time, double sc_pathlength, double cut_beta);
-	static double delta_t_calc(double electron_vertex_time, double mass, double momentum, double sc_t, double sc_r);
-	
-};
-
-/*
 double vertex_time(double sc_time, double sc_pathlength, double cut_beta) {
 	return sc_time - sc_pathlength/(cut_beta * c_special_units);
 }
@@ -27,5 +17,5 @@ double vertex_time(double sc_time, double sc_pathlength, double cut_beta) {
 double delta_t(double electron_vertex_time, double mass, double momentum, double sc_t, double sc_r){
 	double cut_beta = 1.0/sqrt(1.0 + Square(mass/momentum));
 	return electron_vertex_time - vertex_time(sc_t,sc_r,cut_beta);
-} */
+}
 #endif
--- current/delta_t_cut.hpp
+++ current/delta_t_cut.hpp
@@ -38,16 +38,16 @@ void delta_t_cut(){
 	TVector3 Particle3(0.0,0.0,0.0);
 	TLorentzVector Particle4(0.0,0.0,0.0,0.0);
 	double delta_t_P, delta_t_PIP, delta_t_ELECTRON;
-	double electron_vertex = delta_t::vertex_time(sc_t[sc[0]-1], sc_r[sc[0]-1], 1.0);
+	double electron_vertex = vertex_time(sc_t[sc[0]-1], sc_r[sc[0]-1], 1.0);
 
 	for(int event_number = 0; event_number < gpart; event_number++){
 		//Get particles 3 and 4 vector for current event.
 		Particle3.SetXYZ(p[event_number]*cx[event_number], p[event_number]*cy[event_number], p[event_number]*cz[event_number]);
 		Particle4.SetVectM(Particle3,Get_Mass(id[event_number]));
 
-		delta_t_P = delta_t::delta_t_calc(electron_vertex, MASS_P, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
-		delta_t_PIP = delta_t::delta_t_calc(electron_vertex, MASS_PIP, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
-		delta_t_ELECTRON = delta_t::delta_t_calc(electron_vertex, MASS_E, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
+		delta_t_P = delta_t(electron_vertex, MASS_P, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
+		delta_t_PIP = delta_t(electron_vertex, MASS_PIP, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
+		delta_t_ELECTRON = delta_t(electron_vertex, MASS_E, p[event_number], sc_t[sc[event_number]-1], sc_r[sc[event_number]-1]);
 
 		if (event_number == 0 && id[0] == ELECTRON && gpart > 0 && stat[0] > 0 && (int)q[0] == -1 && sc[0] > 0 && dc[0] > 0 && ec[0] > 0 && dc_stat[dc[0]-1] > 0) {
 			delta_t_Fill(Particle4.P(), delta_t_PIP, 8);
