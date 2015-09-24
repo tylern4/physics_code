d5cd76854c9741a71a197ff4797be8e1818c615d
Nick Tyler <tylern@canisius.edu>
2015-09-24 15:23:10 -0400

    rewriting my program to work with classes

---
+++ current/Classes/delta_t.hpp
@@ -0,0 +1,2 @@
+inline double vertex_time(double sc_time, double sc_pathlength, double cut_beta);
+inline double delta_t(double electron_vertex_time, double mass, double momentum, double sc_t, double sc_r);
---
+++ current/Classes/test.mk
@@ -0,0 +1,18 @@
+ROOTLIBS	= $(shell root-config --libs)
+INCLUDE		= $(shell root-config --incdir)
+ROOTSYS		= $(shell root-config --exec-prefix)
+CXX = g++
+CXXFLAGS =      -O2 -fPIC -w -g $(shell root-config --cflags) 
+INCS =          -I$(shell root-config --incdir)
+LIBS =          $(shell root-config --glibs)
+TARGET =	    e1d
+SRC =		$(wildcard *.cpp)
+FILENAME=	$(SRC:.cpp=.o)
+
+all:	clean E1D
+
+E1D:	$(FILENAME) 
+	$(CXX) $(FILENAME) -L. $(CXXFLAGS) $(LIBS) -o $(TARGET) 
+
+clean:
+	-rm -f $(OBJS) $(LIBOUT) $(TARGET) $(FILENAME)
