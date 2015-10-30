93b3fb0217006c6dacdec7c36f0f1e2872eb11af
Nick Tyler <tylern@canisius.edu>
2015-10-30 11:50:20 -0400

    Added missing mass

--- python/ConvertToHDF5.py
+++ python/ConvertToHDF5.py
@@ -8,7 +8,7 @@ from root_numpy import root2rec
 from StringIO import StringIO
 from ROOT import TLorentzVector
 from ROOT import TVector3
-from physics import Q2_calc, W_calc, branches, masses, getM2
+from physics import Q2_calc, W_calc, branches, masses, mass, getM2, missing_mass_calc
 
 lines = [line.rstrip('\n') for line in open(str(sys.argv[1]))]
 e_mu = TLorentzVector(0.0,0.0,np.sqrt(4.802**2.0 - 0.000511**2.0),4.802) #(0.0,0.0, sqrt(Square(E1D_E0)-Square(MASS_E)), E1D_E0)
@@ -17,6 +17,18 @@ for line in lines:
 	print fileNames
 	
 	df = pd.DataFrame(root2rec(fileNames, treename='h10', branches=branches)) #magic line to create a dataframe
+
+	#electron Identification
+	#df = df[ (df.id[:])[0] == 11 ]
+	#df = df[ df['gpart'][:] > 0 ]
+	#df = df[ df['stat'][:][0] > 0 ]
+	#df = df[ df['q'][:][0] == -1 ]
+	#df = df[ df['sc'][:][0] > 0 ]
+	#df = df[ df['dc'][:][0] > 0 ]
+	#df = df[ df['ec'][:][0] > 0 ]
+	#df = df[ df['dc_stat'][:][df['dc'][:][0]-1] > 0 ]
+
+
 	#These lines add a column to the data frame with 'name' = value, should be able to find W, Q2 for an event and add it like this
 	df['pz'] = df.p*df.cz 
 	df['py'] = df.p*df.cy
@@ -26,23 +38,41 @@ for line in lines:
 	#more complicated calcs and if statement ones can be added like this
 	Q2 = []
 	W = []
+	MM = []
 	np.array(Q2)
 	np.array(W)
+	np.array(MM)
+	gamma_mu = TLorentzVector()
+	pi_mu = TLorentzVector()
 	for i in xrange(len(df)):
 		#df['stat'][0] > 0 and df['q'][0] == -1 and df['sc'][0] > 0 and df['dc'][0] > 0 and df['ec'][0] > 0 and df['dc_stat'][df['dc'][0]-1] > 0:
-		if df['id'][i][0] == 11 and df['gpart'][i] > 0:
+		if df['id'][i][0] == 11 and df['gpart'][i] > 1:
 			e_mu_prime_3 = TVector3(df.px[i][0],df.py[i][0],df.pz[i][0])	
 			e_mu_prime = TLorentzVector()
-			e_mu_prime.SetVectM(e_mu_prime_3, 0.000511)
+			e_mu_prime.SetVectM(e_mu_prime_3, mass['ELECTRON'])
 			Q2.append(Q2_calc(e_mu, e_mu_prime))
 			W.append(W_calc(e_mu,e_mu_prime))
+
+			if df['id'][i][1] == 211:
+				gamma_mu = (e_mu - e_mu_prime)
+				pi_mu_3 = TVector3(df.px[i][1],df.py[i][1],df.pz[i][1])
+				pi_mu.SetVectM(pi_mu_3, mass['PIP'])
+				mm = missing_mass_calc(gamma_mu, pi_mu)
+				MM.append(mm)
+			else:
+				MM.append(np.NaN)
 		else:
 			Q2.append(np.NaN)
 			W.append(np.NaN)
+			MM.append(np.NaN)
+
 	df['Q2'] = pd.Series(Q2, index=df.index)
 	df['W'] = pd.Series(W, index=df.index)
+	df['MM'] = pd.Series(MM, index=df.index)
 
 	df = df[~(df.Q2 != df.Q2)]
+
+
 
 	fileNames = line.replace('root', 'h5')
 	os.remove(fileNames) if os.path.isfile(fileNames) else None
