a8f9c0ce911b1ef9adb0706a871ee0d737879f73
Nick Tyler <tylern@canisius.edu>
2016-05-29 17:32:56 -0400

    Switch EC x and y axis

--- current/ec_hist.hpp
+++ current/ec_hist.hpp
@@ -15,16 +15,16 @@
 int bins_EC = 500;
 int bins_p_ec = 500;
 float P_ec_min = 0;
-float P_ec_max = 1;
+float P_ec_max = 5;
 float EC_min = 0;
-float EC_max = 5;
+float EC_max = 1;
 
 TH2D *EC_sampling_fraction = new TH2D("EC_sampling_fraction","EC_sampling_fraction", 
 	bins_p_ec, P_ec_min, P_ec_max, bins_EC, EC_min, EC_max);
 
 void EC_fill(double etot, double momentum){
 	double sampling_frac = etot/momentum;
-	EC_sampling_fraction->Fill(sampling_frac,momentum);
+	EC_sampling_fraction->Fill(momentum,sampling_frac);
 }
 
 void EC_Write(){
