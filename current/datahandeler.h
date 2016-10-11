6da743ea9643c03b7eb15fee50c335cc860bd293
Nick Tyler <tylern@canisius.edu>
2016-10-11 09:28:09 -0400

    Change to three sigma cut

--- current/datahandeler.h
+++ current/datahandeler.h
@@ -61,7 +61,7 @@ void dataHandeler(char *fin, char *RootFile_output, bool first_run){
 
 	for (int current_event = 0; current_event < num_of_events; current_event++) {
 		//update loadbar and get current event
-		loadbar(current_event,num_of_events);
+		loadbar(current_event+1,num_of_events);
 		chain.GetEntry(current_event);
 
 		//reset electron cut bool
