1da9bbbf6e436c83af6ee76a5cf3f5069b56bd5d
Nick Tyler <tylern@canisius.edu>
2016-10-11 12:54:29 -0400

    Change to 3 sigma cut

--- skim/fit_functions.hpp
+++ skim/fit_functions.hpp
@@ -9,6 +9,24 @@
 #define FIT_FUNCTIONS_H_GUARD
 #include "main.h"
 
+
+double Proton_Pos_fit(double x){
+	return 32.9807 * exp(-7.3718 * x) + 0.598541 * x + 0.566383;
+}
+
+double Proton_Neg_fit(double x){
+	return -726.079 * exp(-17.4583 * x) + -0.12433 * x + -1.27606;
+}
+
+double Pip_Pos_fit(double x){
+	return 5.64182 * exp(-3.44168 * x) + 1.41806 * x + -0.182186;
+}
+
+double Pip_Neg_fit(double x){
+	return -6.1602 * exp(-2.6079 * x) + -2.56975 * x + 1.65591;
+}
+
+/* Two Sigma cut from v2_all.lis
 double Proton_Pos_fit(double x){
 	return 8.18213 * exp(-4.47734 * x) + 0.452738 * x +0.233145;
 }
@@ -24,7 +42,7 @@ double Pip_Pos_fit(double x){
 double Pip_Neg_fit(double x){
 	return -3.92061 * exp(-2.58462 * x) + -1.74244 * x + 1.08991;
 }
-
+*/
 
 
 #endif
