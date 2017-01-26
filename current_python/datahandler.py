8f8f7c9fad3fd679d797d83d82cfc953d50aa562
Nick Tyler <tylern@canisius.edu>
2017-01-26 17:48:45 -0500

    Trying to pass full chain with test

--- current_python/datahandler.py
+++ current_python/datahandler.py
@@ -21,8 +21,10 @@ import pandas as pd
 from ROOT import gSystem
 # load library with MyClass dictionary
 gSystem.Load( 'kinematics_C' )
+gSystem.Load( 'test_C' )
 # get MyClass from ROOT
 from ROOT import kinematics
+from ROOT import test
 
 class datahandeler(object):
 	"""Datahandeler class"""
@@ -39,37 +41,28 @@ class datahandeler(object):
 		return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
 				for i in range(wanted_parts) ]
 
-	def run(self):
-		chain = ROOT.TChain('h10')
-		fnum = chain.Add(self.args.input + '*.root')
-		print("Processing "+ str(chain.GetEntries())+ " events in "+ str(fnum) + " files")
-		kine = kinematics()
-		for _e in chain:
-			if _e.id[0] is ID['ELECTRON']:
-				#e_mu_p = fourvec(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0],mass['ELECTRON'])
-				kine.SetVal(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0])
-				self.Q2 = append(self.Q2,kine.GetQ2())
-				self.W = append(self.W,kine.GetW())
-
 	def _run(self, files):
 		chain = ROOT.TChain('h10')
+		kine = kinematics()
+		t = test()
 		for _f in files:
 			chain.Add(_f)
-		kine = kinematics()
-		for _e in chain:
-			if _e.id[0] is ID['ELECTRON']:
-				#e_mu_p = fourvec(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0],mass['ELECTRON'])
-				kine.SetVal(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0])
-				self.Q2 = append(self.Q2,kine.GetQ2())
-				self.W = append(self.W,kine.GetW())
-			print("Hi")
-		WQ2 = pd.DataFrame()
-		WQ2['W'] = self.W
-		WQ2['Q2'] = self.Q2
 
-		name = str(self.args.output+'WQ2_'+str(mp.current_process().pid)+'.pkl')
-		pl.dump(WQ2, open(name, "wb"),protocol=2)
+		t.pass_chain(chain)
+		#for _e in chain:
+			#if _e.id[0] is ID['ELECTRON']:
+			#	#e_mu_p = fourvec(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0],mass['ELECTRON'])
+			#	kine.SetVal(_e.p[0],_e.cx[0],_e.cy[0],_e.cz[0])
+			#	self.Q2 = append(self.Q2,kine.GetQ2())
+			#	self.W = append(self.W,kine.GetW())
+		#WQ2 = pd.DataFrame()
+		#WQ2['W'] = self.W
+		#WQ2['Q2'] = self.Q2
+
+		#name = str(self.args.output+'WQ2_'+str(mp.current_process().pid)+'.pkl')
+		#pl.dump(WQ2, open(name, "wb"),protocol=2)
 		del kine
+		del t
 
 
 	def run_mp(self):
