f887ec163209693352893933b2fafe3d4a44a69a
Nick Tyler <tylern@canisius.edu>
2017-01-31 11:47:37 -0500

    remove P_beta because it's too big

--- current_python/datahandler.py
+++ current_python/datahandler.py
@@ -52,20 +52,11 @@ class datahandeler(object):
 			chain.Add(_f)
 		h10.loop(chain)
 		print("Done with loop "+str(mp.current_process().pid))
-		#_p_beta['p'] = [_p for _p in h10.p_vec]
-		#_p_beta['b'] = [_b for _b in h10.b_vec]
-		#_p_beta['q'] = [_q for _q in h10.q_vec]
-		#_p_beta['id'] = [_id for _id in h10.id_vec]
-		#pl.dump(_p_beta, open(self.args.output + 'p_beta_'+str(mp.current_process().pid)+'.pkl','wb'))
-		#del _p_beta 
-		#print("Done with _p_beta "+str(mp.current_process().pid))
-		#_W_Q2['W'] = [_W for _W in h10.W_vec]
-		#_W_Q2['Q2'] = [_Q2 for _Q2 in h10.Q2_vec]
-		#pl.dump(_W_Q2, open(self.args.output + 'W_Q2_'+str(mp.current_process().pid)+'.pkl','wb'))
-		#del _W_Q2
-		#print("Done with _W_Q2 "+str(mp.current_process().pid))
-		#del h10
-		#print("Done with "+str(mp.current_process().pid))
+		_W_Q2['W'] = [_W for _W in h10.W_vec]
+		_W_Q2['Q2'] = [_Q2 for _Q2 in h10.Q2_vec]
+		pl.dump(_W_Q2, open(self.args.output + 'W_Q2_'+str(mp.current_process().pid)+'.pkl','wb'))
+		del _W_Q2
+		print("Done with _W_Q2 "+str(mp.current_process().pid))
 		return h10
 
 
@@ -76,29 +67,6 @@ class datahandeler(object):
 		out = pool.map(self._run, (files))
 		pool.close()
 		pool.join()
-		i = 0
-		for _h10 in out:
-			i = i+1
-			print("Doing loop "+str(i))
-			_p_beta = pd.DataFrame()
-			_p_beta['p'] = [_p for _p in _h10.p_vec]
-			_p_beta['cx'] = [_p for _p in _h10.cx_vec]
-			_p_beta['cy'] = [_p for _p in _h10.cy_vec]
-			_p_beta['cz'] = [_p for _p in _h10.cz_vec]
-			_p_beta['vx'] = [_p for _p in _h10.vx_vec]
-			_p_beta['vy'] = [_p for _p in _h10.vy_vec]
-			_p_beta['vz'] = [_p for _p in _h10.vz_vec]
-			_p_beta['b'] = [_b for _b in _h10.b_vec]
-			_p_beta['q'] = [_q for _q in _h10.q_vec]
-			_p_beta['id'] = [_id for _id in _h10.id_vec]
-			pl.dump(_p_beta, open(self.args.output + 'p_beta_'+str(i)+'.pkl','wb'),2)
-			del _p_beta
-			_W_Q2 = pd.DataFrame()
-			_W_Q2['W'] = [_W for _W in _h10.W_vec]
-			_W_Q2['Q2'] = [_Q2 for _Q2 in _h10.Q2_vec]
-			pl.dump(_W_Q2, open(self.args.output + 'W_Q2_'+str(i)+'.pkl','wb'),2)
-			del _W_Q2
-
 		#self.W_Q2 = pd.concat(out, ignore_index=False)
 
 	def plot(self):
