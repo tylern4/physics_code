7489ca4fc9b9e555b8953ed5ac18aeecb87779e9
Nick Tyler <tylern@canisius.edu>
2017-02-23 13:22:40 -0500

    update to test

--- current/python/test.py
+++ current/python/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import ROOT
 from ROOT import gROOT, gBenchmark
-
+from pathos.multiprocessing import Pool, cpu_count, ProcessingPool
 import numpy as np
 
 from physics import *
@@ -16,31 +16,44 @@ gROOT.SetBatch(True)
 # Make a chain to load in files, looking at the h10 branch in each file
 chain = ROOT.TChain('h10')
 # Load the files, Can also use glob to get all files in the folder.
-num_files = chain.Add('/Users/tylern/Desktop/dumb/r22855_00.root')
+num_files = chain.Add('/home/tylerns/data2/e1d/v2/root/*.root')
 
 # Make the Q^2 and W arrays that will be filled.
 Q2 = np.array([])
 W = np.array([])
-
-# Start of ROOT based benchmark for testing
-gBenchmark.Start('Python calcs')
-# Loop over all events that have been added to the chain
+evnt_list = []
 for evnt in chain:
-    # If the events ID == ELECTRON then calcuate W and Q^2
+    evnt_list.append(evnt)
+
+
+def calc(evnt):
     if evnt.id[0] is get_id['ELECTRON']:
-        # Make electron four vector
         e_mu_p = fourvec(evnt.p[0], evnt.cx[0],
                          evnt.cy[0], evnt.cz[0], get_mass('ELECTRON'))
-        # Calcuate W and Q^2 and append them to the arrays
-        # Calcuations for W and Q^2 found in physics.py
         Q2 = append(Q2, Q2_calc(e_mu, e_mu_p))
         W = append(W, W_calc(e_mu, e_mu_p))
-gBenchmark.Show('Python calcs')
 
+pool = ProcessingPool()
+pool.map(calc, (evnt_list))
+# Start of ROOT based benchmark for testing
+# gBenchmark.Start('Python calcs')
+# Loop over all events that have been added to the chain
+# for evnt in chain:
+# If the events ID == ELECTRON then calcuate W and Q^2
+#    if evnt.id[0] is get_id['ELECTRON']:
+# Make electron four vector
+#        e_mu_p = fourvec(evnt.p[0], evnt.cx[0],
+#                         evnt.cy[0], evnt.cz[0], get_mass('ELECTRON'))
+# Calcuate W and Q^2 and append them to the arrays
+# Calcuations for W and Q^2 found in physics.py
+#        Q2 = append(Q2, Q2_calc(e_mu, e_mu_p))
+#        W = append(W, W_calc(e_mu, e_mu_p))
+# gBenchmark.Show('Python calcs')
 
+'''
 # Reset everying to do c++ based calculations to compare
-del W
-del Q2
+# del W
+# del Q2
 
 # Make the Q^2 and W arrays that will be filled.
 Q2 = np.array([])
@@ -60,3 +73,4 @@ for evnt in chain:
         Q2 = append(Q2, physics.Q2_calc(e_mu, e_mu_p))
         W = append(W, physics.W_calc(e_mu, e_mu_p))
 gBenchmark.Show('CPP calcs')
+'''
