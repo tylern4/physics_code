911b2dc72ab0e1976f72eda7d011f694df3f39d2
Nick Tyler <tylern@canisius.edu>
2017-02-25 19:08:20 -0500

    trying to check size of objects

--- current/python/datahandler.py
+++ current/python/datahandler.py
@@ -24,6 +24,29 @@ except:
 import cppyy
 cppyy.load_reflection_info("H10.so")
 
+import sys
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects in bytes"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum((get_size(v, seen) for v in obj.values()))
+        size += sum((get_size(k, seen) for k in obj.keys()))
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum((get_size(i, seen) for i in obj))
+    return size
+
 
 class datahandeler(object):
     """Datahandeler class"""
@@ -78,6 +101,7 @@ class datahandeler(object):
         # pl.dump(Q2, open(self.args.output + 'Q2_' +
         #                 str(mp.current_process().pid) + '.pkl', 'wb'), 2)
         gBenchmark.Show("loop " + str(mp.current_process().pid))
+        print("Size: ", get_size(h10))
         return h10
 
     def run_map(self):
