0ca02164340f2c545c8907f1f304baf9a6351866
tylern4 <tylern@canisius.edu>
2017-04-18 15:13:16 -0400

    Updated sim.sh

--- current/simulations/sim.sh
+++ current/simulations/sim.sh
@@ -5,7 +5,6 @@ declare -x CLAS_CALDB_HOST=$CLASDB_PORT_3306_TCP_ADDR
 declare -x CLAS_CALDB_PASS=""
 declare -x CLAS_CALDB_RUNINDEX="RunIndex"
 declare -x RECSIS_RUNTIME="/clas/parms/recsis/runtime"
-#declare -x CLAS_CALDB_USER=$CLASDB_ENV_MYSQL_USER
 
 function noisyexe {
     # Prints the command to stdout before running
@@ -13,10 +12,10 @@ function noisyexe {
     $(echo $@)
 }
 
-#BASE=/root/code
-#DATA=/root/data
-DATA=.
-BASE=.
+CODE=/root/code
+DATA=/root/data
+#DATA=.
+#BASE=.
 VOL=/root
 
 BLUE="\033[34m"
@@ -26,39 +25,45 @@ RED="\033[31m"
 GREEN="\033[32m"
 
 echo -e "$RED#################$DEF"
-aao_rad < $BASE/aao_rad.inp
-part2mctk aao_rad.evt $DATA/thrown.bos
-nt10maker -t2 -o$DATA/thrown.hbook $DATA/thrown.bos
-h2root $DATA/thrown.hbook $DATA/thrown.root
+aao_rad < $CODE/aao_rad.inp
+mv aao_rad.evt $DATA/aao_rad.evt
+part2mctk $DATA/aao_rad.evt $DATA/aao_rad.bos
+h10maker -m $DATA/aao_rad.bos $DATA/aao_rad.root
+
+h10maker -p $DATA/aao_rad.evt $DATA/aao_rad_evt_p.root
 echo -e "$RED#################$DEF"
 
 echo -e "$BLUE##########$DEF"
-gsim_bat -nomcdata -ffread $BASE/gsim.inp -mcin $DATA/thrown.bos -kine 1 -bosout $DATA/gsim.bos
-cp $DATA/gsim.bos $DATA/gsim_uncooked.bos
-cp $DATA/gsim.bos $DATA/uncooked.bos
-noisyexe user_ana -t user_ana.tcl
-rm anamonhist logfile
-rm $DATA/uncooked.bos
-rm $DATA/gsim.bos
-mv cooked.bos $DATA/gsim.bos
-mv cooked_chist.hbook $DATA/gsim_chist.hbook
-nt10maker -t3 $DATA/gsim.bos -o$DATA/gsim.hbook
-h2root $DATA/gsim.hbook $DATA/gsim.root
-h2root $DATA/gsim_chist.hbook $DATA/gsim_chist.root
+gsim_bat -nomcdata -ffread $CODE/gsim.inp -mcin $DATA/aao_rad.bos -kine 1 -bosout $DATA/gsim.bos
+h10maker -m $DATA/gsim.bos $DATA/gsim_m.root
+
+gsim_bat -nomcdata -ffread $CODE/gsim.inp -mcin $DATA/aao_rad.evt -kine 1 -bosout $DATA/gsim_evt.bos
+h10maker -p $DATA/gsim_evt.bos $DATA/gsim_evt_p.root
+#cp gsim.bos uncooked.bos
+#noisyexe user_ana -t user_ana.tcl 2> user_ana_1.err 1> user_ana_1.out
+#h10maker -r cooked.bos cooked_1.root
+#h2root cooked_chist.hbook cooked_chist_1.root
+#rm -r uncooked.bos cooked.bos cooked_chist.hbook
+#cp gsim_evt.bos uncooked.bos
+#noisyexe user_ana -t user_ana.tcl 2> user_ana_2.err 1> user_ana_2.out
+#h10maker -r cooked.bos cooked_2.root
+#h2root cooked_chist.hbook cooked_chist_2.root
+#rm -r uncooked.bos cooked.bos cooked_chist.hbook
 echo -e "$BLUE##########$DEF"
 
 echo -e "$GREEN##########$DEF"
-export CLAS_CALDB_RUNINDEX=calib_user.RunIndexe1_6
-cp gsim_uncooked.bos gpp_in.bos
-gpp gsim.inp
-rm gpp_in.bos
-mv gpp_out.bos reconstructed_GPP_OUT
-splitbos -runnum 10 -o reconstructed_gpp_split_out.bos reconstructed_GPP_OUT
-cp reconstructed_gpp_split_out.bos uncooked.bos
-user_ana -t user_ana.tcl
-mv cooked_chist.hbook reconstructed_chist.hbook
-mv cooked.bos reconstructed.bos
-nt10maker -t3 reconstructed.bos -oreconstructed.hbook
-h2root reconstructed.hbook reconstructed.root
-h2root reconstructed_chist.hbook reconstructed_chist.root
+##export CLAS_CALDB_RUNINDEX=calib_user.RunIndexe1_6
+##cat gpp.inp | grep -v '^#.*' | xargs gpp
+#cp gsim_uncooked.bos gpp_in.bos
+#gpp gsim.inp
+#rm gpp_in.bos
+#mv gpp_out.bos reconstructed_GPP_OUT
+#splitbos -runnum 10 -o reconstructed_gpp_split_out.bos reconstructed_GPP_OUT
+#cp reconstructed_gpp_split_out.bos uncooked.bos
+#user_ana -t user_ana.tcl
+#mv cooked_chist.hbook reconstructed_chist.hbook
+#mv cooked.bos reconstructed.bos
+#nt10maker -t3 reconstructed.bos -oreconstructed.hbook
+#h2root reconstructed.hbook reconstructed.root
+#h2root reconstructed_chist.hbook reconstructed_chist.root
 echo -e "$GREEN##########$DEF"
