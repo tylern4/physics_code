009745a5c556b1a9198c27af0dd35356ba43a1be
Nick Tyler <tylern@canisius.edu>
2017-08-24 13:25:43 -0400

    Add csv maker for electron events

---
+++ current/csv_maker/csv_maker.cpp
@@ -0,0 +1,32 @@
+/************************************************************************/
+/*  Created by Nick Tyler*/
+/*	University Of South Carolina*/
+/************************************************************************/
+
+// Only My Includes. All others in main.h
+#include "main.h"
+#include "../src/classes.hpp"
+#include "../src/constants.hpp"
+#include "../src/branches.hpp"
+#include "TStopwatch.h"
+#include "../src/physics.hpp"
+#include "csv_maker.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  TStopwatch *Watch = new TStopwatch;
+  Watch->Start();
+  gStyle->SetOptFit(1111);
+
+  if (argc == 3) {
+    char *infilename = argv[1];
+    char *outfilename = argv[2];
+    make_electron_csv(infilename, outfilename);
+  }
+
+  Watch->Stop();
+  cout << red << Watch->RealTime() << "sec" << def << endl;
+
+  return 0;
+}
