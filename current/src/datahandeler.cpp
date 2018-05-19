cc89d8ad0b46da2c55f403ab98c9eafa5bd8aeae
Nicholas Tyler <tylern@canisius.edu>
2018-05-19 15:50:20 -0400

    Still no work

--- current/src/datahandeler.cpp
+++ current/src/datahandeler.cpp
@@ -145,9 +145,8 @@ void DataHandeler::run() {
 
 #pragma omp parallel for private(i)
   for (i = 0; i < size; i++) {
-    std::cout << "Hi I'm parallel" << std::endl;
     // loadbar(i + 1, size);
-    // file_handeler(input_files.at(i));
+    file_handeler(input_files.at(i));
   }
 }
 
@@ -169,125 +168,128 @@ void DataHandeler::file_handeler(std::string fin) {
   getBranches(chain);
   if (!first_run) getMorebranchs(chain);
   num_of_events = (int)chain->GetEntries();
+  std::cout << "Hi I'm parallel " << num_of_events << std::endl;
+  /*
+    int current_event = 0;
+    for (current_event = 0; current_event < num_of_events; current_event++) {
+      chain->GetEntry(current_event);
 
-  int current_event = 0;
-  for (current_event = 0; current_event < num_of_events; current_event++) {
-    chain->GetEntry(current_event);
-
-    // reset electron cut bool
-    electron_cuts = true;
-    // electron cuts
-    electron_cuts &= (ec[0] > 0);                                  // ``` ``` ``` ec
-    electron_cuts &= ((int)id[0] == ELECTRON || (int)id[0] == 0);  // First particle is electron
-    electron_cuts &= ((int)gpart > 0);                             // Number of good particles is greater than 0
-    electron_cuts &= ((int)stat[0] > 0);                           // First Particle hit stat
-    electron_cuts &= ((int)q[0] == -1);                            // First particle is negative Q
-    electron_cuts &= ((int)sc[0] > 0);                             // First Particle hit sc
-    electron_cuts &= ((int)dc[0] > 0);                             // ``` ``` ``` dc
-    electron_cuts &= ((int)dc_stat[dc[0] - 1] > 0);
-    electron_cuts &= ((int)cc[0] > 0);
-    if (electron_cuts) hists->EC_fill(etot[ec[0] - 1], p[0]);
-    electron_cuts &= (p[0] > MIN_P_CUT);  // Minimum Momentum cut????
-
-    if (electron_cuts) {
-      int cc_sector = cc_sect[cc[0] - 1];
-      int cc_segment = (cc_segm[0] % 1000) / 10;
-      int cc_pmt = cc_segm[0] / 1000 - 1;
-      int cc_nphe = nphe[cc[0] - 1];
-      double theta_cc = TMath::ACos(TMath::Abs(p[0] * cz[0]) / TMath::Abs(p[0]));
-
-      theta_cc = theta_cc / D2R;
-      hists->CC_fill(cc_sector, cc_segment, cc_pmt, cc_nphe, theta_cc);
-
-      hists->Fill_Beam_Position((double)dc_vx[dc[0] - 1], (double)dc_vy[dc[0] - 1], (double)dc_vz[dc[0] - 1]);
-      if (first_run) {
-        is_electron = elec_vec;
-        is_electron->at(0) = true;
-        for (int part_num = 1; part_num < gpart; part_num++) {
-          is_pip = pip_vec;
-          is_pim = pim_vec;
-          is_proton = proton_vec;
-          is_pip->at(part_num) = (id[part_num] == PIP);
-          is_proton->at(part_num) = (id[part_num] == PROTON);
-          is_pim->at(part_num) = (id[part_num] == PIM);
-        }
-      }
-
-      // Setup scattered electron 4 vector
-      TLorentzVector e_mu_prime = physics::fourVec(p[0], cx[0], cy[0], cz[0], MASS_E);
-
-      // Set the vertex time (time of electron hit)
-      Delta_T *dt = new Delta_T(sc_t[sc[0] - 1], sc_r[sc[0] - 1]);
-      dt->delta_t_hists(hists);
-      // std::vector<double> dt_proton = dt->delta_t_array(MASS_P, gpart);
-      // std::vector<double> dt_pi = dt->delta_t_array(MASS_PIP, gpart);
-      delete dt;
+      // reset electron cut bool
+      electron_cuts = true;
+      // electron cuts
+      electron_cuts &= (ec[0] > 0);                                  // ``` ``` ``` ec
+      electron_cuts &= ((int)id[0] == ELECTRON || (int)id[0] == 0);  // First particle is electron
+      electron_cuts &= ((int)gpart > 0);                             // Number of good particles is greater than 0
+      electron_cuts &= ((int)stat[0] > 0);                           // First Particle hit stat
+      electron_cuts &= ((int)q[0] == -1);                            // First particle is negative Q
+      electron_cuts &= ((int)sc[0] > 0);                             // First Particle hit sc
+      electron_cuts &= ((int)dc[0] > 0);                             // ``` ``` ``` dc
+      electron_cuts &= ((int)dc_stat[dc[0] - 1] > 0);
+      electron_cuts &= ((int)cc[0] > 0);
+      if (electron_cuts) hists->EC_fill(etot[ec[0] - 1], p[0]);
+      electron_cuts &= (p[0] > MIN_P_CUT);  // Minimum Momentum cut????
 
       if (electron_cuts) {
-        theta = physics::theta_calc(cz[0]);
-        phi = physics::phi_calc(cx[0], cy[0]);
-        sector = physics::get_sector(phi);
+        int cc_sector = cc_sect[cc[0] - 1];
+        int cc_segment = (cc_segm[0] % 1000) / 10;
+        int cc_pmt = cc_segm[0] / 1000 - 1;
+        int cc_nphe = nphe[cc[0] - 1];
+        double theta_cc = TMath::ACos(TMath::Abs(p[0] * cz[0]) / TMath::Abs(p[0]));
 
-        hists->Fill_electron_fid(theta, phi, sector);
-      }
-      if (first_run) {
-        W = physics::W_calc(*e_mu, e_mu_prime);
-        Q2 = physics::Q2_calc(*e_mu, e_mu_prime);
-        e_E = e_mu_prime.E();
-      }
+        theta_cc = theta_cc / D2R;
+        hists->CC_fill(cc_sector, cc_segment, cc_pmt, cc_nphe, theta_cc);
 
-      hists->WvsQ2_Fill(e_E, W, Q2, physics::xb_calc(Q2, e_E));
-      num_of_proton = num_of_pis = 0;
-      for (int part_num = 1; part_num < gpart; part_num++) {
-        if (p[part_num] == 0) continue;
-        if (is_proton->at(part_num) == is_pip->at(part_num)) continue;
-
-        theta = physics::theta_calc(cz[part_num]);
-        phi = physics::phi_calc(cx[part_num], cy[part_num]);
-        sector = physics::get_sector(phi);
-        hists->Fill_hadron_fid(theta, phi, sector, id[part_num]);
-
-        hists->Fill_Mass(m[part_num]);
-        TLorentzVector Particle = physics::fourVec(p[part_num], cx[part_num], cy[part_num], cz[part_num], id[part_num]);
-
-        hists->MomVsBeta_Fill(Particle.E(), p[part_num], b[part_num]);
-        if (q[part_num] == 1) {
-          hists->MomVsBeta_Fill_pos(p[part_num], b[part_num]);
-          if (is_proton->at(part_num) && (id[part_num] == PROTON)) {
-            num_of_proton++;
-            hists->Fill_proton_WQ2(W, Q2);
-            hists->Fill_proton_ID_P(p[part_num], b[part_num]);
-          } else if (is_pip->at(part_num) && (id[part_num] == PIP)) {
-            num_of_pis++;
-            hists->Fill_pion_WQ2(W, Q2);
-            hists->Fill_Pi_ID_P(p[part_num], b[part_num]);
-            TLorentzVector gamma_mu = (*e_mu - e_mu_prime);
-            if (first_run) {
-              MM_neutron->Set_PxPyPz(p[part_num] * cx[part_num], p[part_num] * cy[part_num],
-                                     p[part_num] * cz[part_num]);
-              MM_neutron->missing_mass(gamma_mu);
-            }
+        hists->Fill_Beam_Position((double)dc_vx[dc[0] - 1], (double)dc_vy[dc[0] - 1], (double)dc_vz[dc[0] - 1]);
+        if (first_run) {
+          is_electron = elec_vec;
+          is_electron->at(0) = true;
+          for (int part_num = 1; part_num < gpart; part_num++) {
+            is_pip = pip_vec;
+            is_pim = pim_vec;
+            is_proton = proton_vec;
+            is_pip->at(part_num) = (id[part_num] == PIP);
+            is_proton->at(part_num) = (id[part_num] == PROTON);
+            is_pim->at(part_num) = (id[part_num] == PIM);
           }
-
-          if ((is_pip->at(part_num) && (id[part_num] == PIP)) ||
-              (is_proton->at(part_num) && (id[part_num] == PROTON))) {
-            hists->Fill_proton_Pi_ID_P(p[part_num], b[part_num]);
-          }
-        } else if (q[part_num] == -1) {
-          hists->MomVsBeta_Fill_neg(p[part_num], b[part_num]);
         }
-      }
 
-      if (num_of_pis == 1 && gpart < 3) hists->Fill_single_pi_WQ2(W, Q2);
+        // Setup scattered electron 4 vector
+        TLorentzVector e_mu_prime = physics::fourVec(p[0], cx[0], cy[0], cz[0], MASS_E);
 
-      if (num_of_pis == 1 && num_of_proton == 0 && gpart < 3) {
-        hists->Fill_Missing_Mass(MM_neutron->Get_MM());
-        hists->Fill_Missing_Mass_square(MM_neutron->Get_MM2());
+        // Set the vertex time (time of electron hit)
+        Delta_T *dt = new Delta_T(sc_t[sc[0] - 1], sc_r[sc[0] - 1]);
+        dt->delta_t_hists(hists);
+        // std::vector<double> dt_proton = dt->delta_t_array(MASS_P, gpart);
+        // std::vector<double> dt_pi = dt->delta_t_array(MASS_PIP, gpart);
+        delete dt;
+
+        if (electron_cuts) {
+          theta = physics::theta_calc(cz[0]);
+          phi = physics::phi_calc(cx[0], cy[0]);
+          sector = physics::get_sector(phi);
+
+          hists->Fill_electron_fid(theta, phi, sector);
+        }
+        if (first_run) {
+          W = physics::W_calc(*e_mu, e_mu_prime);
+          Q2 = physics::Q2_calc(*e_mu, e_mu_prime);
+          e_E = e_mu_prime.E();
+        }
+
+        hists->WvsQ2_Fill(e_E, W, Q2, physics::xb_calc(Q2, e_E));
+        num_of_proton = num_of_pis = 0;
+        for (int part_num = 1; part_num < gpart; part_num++) {
+          if (p[part_num] == 0) continue;
+          if (is_proton->at(part_num) == is_pip->at(part_num)) continue;
+
+          theta = physics::theta_calc(cz[part_num]);
+          phi = physics::phi_calc(cx[part_num], cy[part_num]);
+          sector = physics::get_sector(phi);
+          hists->Fill_hadron_fid(theta, phi, sector, id[part_num]);
+
+          hists->Fill_Mass(m[part_num]);
+          TLorentzVector Particle = physics::fourVec(p[part_num], cx[part_num], cy[part_num], cz[part_num],
+    id[part_num]);
+
+          hists->MomVsBeta_Fill(Particle.E(), p[part_num], b[part_num]);
+          if (q[part_num] == 1) {
+            hists->MomVsBeta_Fill_pos(p[part_num], b[part_num]);
+            if (is_proton->at(part_num) && (id[part_num] == PROTON)) {
+              num_of_proton++;
+              hists->Fill_proton_WQ2(W, Q2);
+              hists->Fill_proton_ID_P(p[part_num], b[part_num]);
+            } else if (is_pip->at(part_num) && (id[part_num] == PIP)) {
+              num_of_pis++;
+              hists->Fill_pion_WQ2(W, Q2);
+              hists->Fill_Pi_ID_P(p[part_num], b[part_num]);
+              TLorentzVector gamma_mu = (*e_mu - e_mu_prime);
+              if (first_run) {
+                MM_neutron->Set_PxPyPz(p[part_num] * cx[part_num], p[part_num] * cy[part_num],
+                                       p[part_num] * cz[part_num]);
+                MM_neutron->missing_mass(gamma_mu);
+              }
+            }
+
+            if ((is_pip->at(part_num) && (id[part_num] == PIP)) ||
+                (is_proton->at(part_num) && (id[part_num] == PROTON))) {
+              hists->Fill_proton_Pi_ID_P(p[part_num], b[part_num]);
+            }
+          } else if (q[part_num] == -1) {
+            hists->MomVsBeta_Fill_neg(p[part_num], b[part_num]);
+          }
+        }
+
+        if (num_of_pis == 1 && gpart < 3) hists->Fill_single_pi_WQ2(W, Q2);
+
+        if (num_of_pis == 1 && num_of_proton == 0 && gpart < 3) {
+          hists->Fill_Missing_Mass(MM_neutron->Get_MM());
+          hists->Fill_Missing_Mass_square(MM_neutron->Get_MM2());
+        }
+        if (num_of_proton == 1) hists->Fill_single_proton_WQ2(W, Q2);
       }
-      if (num_of_proton == 1) hists->Fill_single_proton_WQ2(W, Q2);
+      // std::cout << "End of loop " << current_event << std::endl;
     }
-    // std::cout << "End of loop " << current_event << std::endl;
-  }
+    */
   chain->Reset();  // delete Tree object
   delete chain;
 }
