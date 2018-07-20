55714b51d4b35d9b63a74cccb91e1fa24dd049b1
Nicholas Tyler <tylern@canisius.edu>
2018-07-20 18:28:04 -0400

    Multicore cython/python skim

--- current/skim/skim.pyx
+++ current/skim/skim.pyx
@@ -1,16 +1,33 @@
-from libcpp.vector cimport vector
 from libcpp.string cimport string
-from libcpp cimport bool
+from libcpp.vector cimport vector
+from multiprocessing import Pool
+import multiprocessing
+from timeit import default_timer as timer
+import glob
 
-cdef extern from "../src/skim.hpp" namespace "Skim":
+
+cdef extern from "skim.hpp":
     cdef cppclass Skim:
       Skim(string) except +
-      Process()
-
+      void Process()
 
 cdef class py_skim:
   cdef Skim*c_skim
+  cdef string f
   def __cinit__(self, filename):
+    self.f = filename
     self.c_skim = new Skim(filename)
-  #def Process():
-  #  self.c_skim.Process()
+    self.c_skim.Process()
+  def __reduce__(self):
+    return (self.__class__, (self.f, ))
+
+class skim_files:
+  def __init__(self, input):
+    self.files = glob.glob(input)
+  def run(self):
+    start = timer()
+    num_cores = multiprocessing.cpu_count()
+    pool = Pool(processes=num_cores)
+    pool.map(py_skim, (self.files))
+    end = timer()
+    print("Time: "+ str(end - start))
