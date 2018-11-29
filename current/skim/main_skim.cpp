4a4cdaa08c09dafc5056e24890ed7ebba94e81f7
Nicholas Tyler <tylern@canisius.edu>
2018-11-29 12:23:40 -0500

    Update skim with clipp

--- current/skim/main_skim.cpp
+++ current/skim/main_skim.cpp
@@ -1,14 +1,13 @@
-/************************************************************************/
+/**************************************/
 /*																		*/
 /*																		*/
-/*  Created by Nick Tyler
- */
-/*	University Of South Carolina
- */
-/************************************************************************/
+/*  Created by Nick Tyler             */
+/*	University Of South Carolina      */
+/**************************************/
 
 // Only My Includes. All others in main.h
 #include "../src/classes.hpp"
+#include "../src/clipp.h"
 #include "../src/glob_files.hpp"
 #include "../src/physics.hpp"
 #include "../src/skim.hpp"
@@ -17,29 +16,37 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  gROOT->SetBatch(true);
-
+  bool _mc;
+  bool _basic;
+  bool _strict;
+  bool _final;
+  bool print_help;
   std::vector<std::string> infile;
   std::string outfile;
-  if (argc == 2) {
-    infile = glob(argv[1]);
-    outfile = infile[0].substr(0, infile[0].size() - 8) + "_skim.root";
-  } else if (argc == 3) {
-    infile = glob(argv[1]);
-    outfile = argv[2];
-  } else {
-    std::cerr << RED << "Error: \n";
-    std::cerr << BOLDRED << "\tNeed input file and output file\n";
-    std::cerr << RESET << "Usage:\n\t";
-    std::cerr << BOLDWHITE << argv[0] << " '/path/to/files/to/skim_*.root'\n\n";
-    std::cerr << RESET << std::endl;
-    return 1;
+  auto cli =
+      (clipp::option("-h", "--help").set(print_help) % "print help", clipp::option("-mc", "--MC").set(_mc) % "mc Skim",
+       clipp::option("-b", "--basic").set(_basic) % "basic skim",
+       clipp::option("-s", "--strict").set(_strict) % "strict skim",
+       clipp::option("-f", "--final").set(_mc) % "final skim", clipp::value("skim.root", outfile),
+       clipp::values("inputFile.root", infile));
+
+  clipp::parse(argc, argv, cli);
+  if (print_help) {
+    for (auto f : infile) std::cout << f << std::endl;
+    std::cout << clipp::make_man_page(cli, argv[0]);
+    exit(-1);
   }
 
   Skim *s = new Skim(infile, outfile);
-  // s->Basic();
-  s->Strict();
-  // s->Final();
+
+  if (_basic)
+    s->Basic();
+  else if (_strict)
+    s->Strict();
+  else if (_final)
+    s->Final();
+  else
+    s->Basic();
 
   return 0;
 }
