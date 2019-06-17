19c4e5c8a13c1a61037144ed0b6ee922924dbd77
Nick Tyler <tylern@canisius.edu>
2019-06-17 13:10:02 -0400

    Working threaded for ntuple_maker

--- current/ntuple_maker/ntuple_maker.cpp
+++ current/ntuple_maker/ntuple_maker.cpp
@@ -14,11 +14,10 @@
 #include "yeilds.hpp"
 
 using namespace std;
-size_t run_file(std::string in, int thread_id) {
-  std::string outf = in.substr(in.find("h10_"));
-  outf = "out/ntuple_" + std::to_string(thread_id) + "_" + outf;
+size_t run_file(std::vector<std::string> in, std::string out_path, int thread_id) {
+  std::string outf = out_path + "/ntuple_" + std::to_string(thread_id) + ".root";
   auto chain = std::make_unique<TChain>("h10");
-  chain->Add(in.c_str());
+  for (auto& f : in) chain->Add(f.c_str());
   auto dh = std::make_unique<Yeilds>(outf.c_str(), true);
   return dh->RunNtuple(std::move(chain));
 }
@@ -27,11 +26,11 @@ void call_from_thread(std::string in) { std::cout << in << std::endl; }
 
 int main(int argc, char** argv) {
   ROOT::EnableThreadSafety();
-  std::vector<std::string> infilename;
+  std::vector<std::vector<std::string>> infilenames(NUM_THREADS);
   std::string outfilename;
   if (argc >= 2) {
     outfilename = argv[1];
-    for (int i = 2; i < argc; i++) infilename.push_back(argv[i]);
+    for (int i = 2; i < argc; i++) infilenames[i % NUM_THREADS].push_back(argv[i]);
   } else {
     return 1;
   }
@@ -41,18 +40,13 @@ int main(int argc, char** argv) {
   size_t events = 0;
   int j = 0;
 
-for (size_t i = 0; i < count; i++) {
-
-}
   std::thread t[NUM_THREADS];
   for (size_t i = 0; i < NUM_THREADS; i++) {
-    t[i] = std::thread(run_file, infilename.at(i), i);
+    t[i] = std::thread(run_file, infilenames.at(i), outfilename, i);
   }
   for (size_t i = 0; i < NUM_THREADS; i++) {
     t[i].join();
   }
-
-  // events += run_file(in);
 
   std::chrono::duration<double> elapsed_full = (std::chrono::high_resolution_clock::now() - start);
   std::cout << RED << elapsed_full.count() << " sec" << DEF << std::endl;
