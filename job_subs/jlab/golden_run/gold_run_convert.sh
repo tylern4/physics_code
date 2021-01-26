fb92833b094df405b5694f1d3fef725e874f8810
tylern4 <tylern@canisius.edu>
2021-01-26 14:04:48 -0500

    Adding golden run convert

---
+++ job_subs/jlab/golden_run/gold_run_convert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#INPUT Sequences file
+
+#SBATCH -J h10maker
+#SBATCH --cpus-per-task=64
+#SBATCH --mem-per-cpu=256
+#SBATCH -N 1
+#SBATCH --output=log/h10maker_%A.out
+#SBATCH --error=log/h10maker_%A.err
+
+
+export NUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUM_THREADS=2
+
+export INPUT_FOLDER=/volatile/clas/clase1/tylern/bos_files
+export OUTPUT_FOLDER=/work/clas/clase1/tylern/e1d/data/golden_run
+
+mkdir -p $OUTPUT_FOLDER
+
+cat golden_run.list | parallel --dry-run -j$NUM_THREADS singularity exec /cvmfs/singularity.opensciencegrid.org/tylern4/clas6:latest h10maker -r $INPUT_FOLDER/{} $OUTPUT_FOLDER/h10_gr_{.}.root
+
+
+echo "DONE!!"
